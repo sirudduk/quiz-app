6cc5c4510a19db4f3c7e528cac8f1ab741f897b9
jeongkyun <jeongkyun@b4play.io>
2022-12-12 18:41:03 +0900

    feat) router 추가 및 구조 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import Quiz from '@/components/quiz';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import StartPage from './pages/Start';
+import QuizPage from './pages/Quiz';
 import Container from '@/components/layout/Container';
 
 function App() {
   return (
     <Container>
-      <Quiz />
+      <Router>
+        <Routes>
+          <Route path="/" element={<StartPage />} />
+          <Route path="/quiz" element={<QuizPage />} />
+        </Routes>
+      </Router>
     </Container>
   );
 }
--- src/components/quiz/index.tsx
+++
@@ -1,9 +0,0 @@
-import Button, { BUTTON_SIZE } from '@/components/ui/Button';
-
-export default function Quiz() {
-  return (
-    <div>
-      <Button title="퀴즈 풀기" size={BUTTON_SIZE.LARGE} />
-    </div>
-  );
-}
