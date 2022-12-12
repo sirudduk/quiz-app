aedb0066005243b671f6255c73f13c824a6127df
jeongkyun <jeongkyun@b4play.io>
2022-12-13 01:45:07 +0900

    feat) quiz interface 추가

---
+++ src/interface/quiz.ts
@@ -0,0 +1,10 @@
+type DifficultyType = 'easy' | 'medium' | 'hard';
+
+export interface QuizInterface {
+  category: string;
+  correct_answer: string;
+  difficulty: DifficultyType;
+  incorrect_answers: string[];
+  question: string;
+  type: string;
+}
