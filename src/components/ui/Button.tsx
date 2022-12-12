e9608bc23992904c9ad1c8e3c462ef2ccfa80a65
jeongkyun <jeongkyun@b4play.io>
2022-12-12 18:41:54 +0900

    feat)button 컴포넌트 수정

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -4,17 +4,24 @@ export enum BUTTON_SIZE {
   LARGE,
 }
 
-export default function Button(props: { title: string; size: BUTTON_SIZE }) {
+interface ButtonProps {
+  title: string;
+  size: BUTTON_SIZE;
+  onClick: () => void;
+}
+
+export default function Button(props: ButtonProps) {
   const size = {
     [BUTTON_SIZE.SMALL]: 'px-4 py-2 text-sm',
     [BUTTON_SIZE.MEDIUM]: 'px-5 py-3 text-m',
     [BUTTON_SIZE.LARGE]: 'px-6 py-4 text-lg',
   }[props.size || BUTTON_SIZE.LARGE];
 
+  const defaultBtnStyles =
+    'border-2 text-gray-600 font-bold border-orange-300 rounded-md';
+
   return (
-    <button
-      className={`border text-gray-600 font-bold border-orange-300 rounded-md ${size}`}
-    >
+    <button className={`${defaultBtnStyles} ${size}`} onClick={props.onClick}>
       {props.title}
     </button>
   );
