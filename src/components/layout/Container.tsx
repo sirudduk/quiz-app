7091af278f8e318c294992241b0f2537cb506ba0
jeongkyun <jeongkyun@b4play.io>
2022-12-14 00:18:53 +0900

    fix) container 컴포넌트 수정

--- src/components/layout/Container.tsx
+++ src/components/layout/Container.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 export default function Container(props: { children: React.ReactNode }) {
   return (
-    <div className="w-full h-full flex justify-center items-center overflow-hidden">
+    <div className="w-full h-full flex justify-center items-center overflow-hidden bg-orange-50 flex-col text-gray-800">
       {props.children}
     </div>
   );
