0b7194aa09868872b11ab55e3624df60f4b44d35
jeongkyun <jeongkyun@b4play.io>
2022-12-14 03:44:48 +0900

    fix) start page 작업

--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -1,12 +1,23 @@
+import { useContext, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+
 import Button, { BUTTON_SIZE } from '@/components/ui/Button';
+import { QuizContext } from '@/store/context';
+import { ActionTypes } from '@/store/types';
 
 export default function StartPage() {
   const navigate = useNavigate();
+  const { dispatch } = useContext(QuizContext);
 
   function handleClick() {
     navigate('/quiz');
   }
+
+  useEffect(() => {
+    dispatch({
+      type: ActionTypes.SET_RESET_QUIZ,
+    });
+  }, []);
 
   return (
     <div>
